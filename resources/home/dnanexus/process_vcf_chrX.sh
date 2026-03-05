037b733eefda8e474af4345b4d52335faf55094d
Kartik Chundru <v.chundru@exeter.ac.uk>
2026-03-05 10:38:52 +0000

    Increase max allele length in process_vcf_chrX.sh

--- resources/home/dnanexus/process_vcf_chrX.sh
+++ resources/home/dnanexus/process_vcf_chrX.sh
@@ -11,7 +11,7 @@ then
 else
 
 dx download "${DX_PROJECT_CONTEXT_ID}:/Bulk/DRAGEN WGS/DRAGEN population level WGS variants, pVCF format [500k release]/${chrom}/${1}"
-./plink2_ukbb_vcftopgen --vcf ${1} --memory ${mem} --geno ${maxmiss} --vcf-min-gq ${mingq} --vcf-min-dp ${mindp} --var-filter --set-all-var-ids "@:#:\$r:\$a" --new-id-max-allele-len 1000 --import-max-alleles 255 --update-sex sex_file.txt --make-pgen multiallelics=- --out ${1::-7}
+./plink2_ukbb_vcftopgen --vcf ${1} --memory ${mem} --geno ${maxmiss} --vcf-min-gq ${mingq} --vcf-min-dp ${mindp} --var-filter --set-all-var-ids "@:#:\$r:\$a" --new-id-max-allele-len 10000 m --import-max-alleles 255 --update-sex sex_file.txt --make-pgen multiallelics=- --out ${1::-7}
 dx upload ${1::-7}.p* --path "${DX_PROJECT_CONTEXT_ID}:${outdir}/${chrom}/"
 rm ${1}
 rm ${1::-7}.p*
